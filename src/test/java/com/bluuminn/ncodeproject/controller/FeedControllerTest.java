65c08bf7d5ac8fb9497f2ca5aa37ac3856db1bda
bluuminn <bluuminn@gmail.com>
2020-03-16 03:56:30 +0900

    FeedControllerTest status 200 테스트 작성

---
+++ src/test/java/com/bluuminn/ncodeproject/controller/FeedControllerTest.java
@@ -0,0 +1,23 @@
+package com.bluuminn.ncodeproject.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FeedController.class)
+class FeedControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void list() throws Exception {
+        mockMvc.perform(get("/feed"))
+                .andExpect(status().isOk());
+    }
+
+}
