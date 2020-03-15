b31274e3e1280f418196aba78937b8068ac332ef
bluuminn <bluuminn@gmail.com>
2020-03-16 03:10:14 +0900

    Initial commit

---
+++ src/main/java/com/bluuminn/ncodeproject/NcodeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bluuminn.ncodeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcodeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcodeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluuminn/ncodeproject/NcodeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluuminn.ncodeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcodeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
