855cf6790f49237fc3a83533d89130334cd6b0e6
bluuminn <bluuminn@gmail.com>
2020-03-16 17:32:15 +0900

    FeedDto에 Mapping 추가

--- src/main/java/com/bluuminn/ncodeproject/dto/FeedDto.java
+++ src/main/java/com/bluuminn/ncodeproject/dto/FeedDto.java
@@ -1,5 +1,6 @@
 package com.bluuminn.ncodeproject.dto;
 
+import com.github.dozermapper.core.Mapping;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,12 +12,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class FeedDto {
 
+    @Mapping("id")
     private Long id;
+
+    @Mapping("mdImages")
     private String mdImages;
+
+    @Mapping("mdName")
     private String mdName;
+
+    @Mapping("contents")
     private String contents;
+
+    @Mapping("countOfComments")
     private Integer countOfComments;
+
+    @Mapping("countOfLikes")
     private Integer countOfLikes;
+
+    @Mapping("countOfShared")
     private Integer countOfShared;
 
 }
